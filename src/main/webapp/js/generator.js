c2dbae0e697668d2aac1f4e327944afc757f7620
electricmind <buy@neural.ru>
2014-05-14 19:54:03 +0400

    Add self-refreshable progress list on the page "load"

--- src/main/webapp/js/generator.js
+++ src/main/webapp/js/generator.js
@@ -1,103 +1,105 @@
-$(document).ready(function() {
-    var cache = [];
-    
-    $(".remove").button().on("click", function() {
-        if ($(".phrase :last .word").size() == 2) {
-            if ($(".phrase").size() > 1) {
-                $(".phrase :last").remove();
-                $(".phrase :last .word :last").remove();
-            } else {
-                return;
-            }
-        } else {
-            $(".phrase :last .word :last").remove();
-        }
-        
-        $(".words").remove();
-        add();        
-    });
+$(document).ready(
+        function() {
+            var cache = [];
 
-    function add() {
-        var url = "/json/words/";
-        var w2 = $(".phrase :last .word :last").data("word");
-        var w1 = $(".phrase :last .word :last").prev().data("word");
-        
-        if (w1 != "") {
-            url += w1 + "/";
-        }
-        if (w2 != "") {
-            url += w2;
-        }
-        
-        request(url);
-    }
-    
-    function request(url, i) {
-
-        i = typeof i !== 'undefined' ? i : 0;
-
-        function update(data) {
-            $("#dropmenu").tmpl({
-                wps : data
-            }).appendTo(".phrase :last");
-            $(".words").menu({
-                position : {
-                    my : "top",
-                    at : "right-5 top+5"
+            $(".remove").button().on("click", function() {
+                if ($(".phrase :last .word").size() == 2) {
+                    if ($(".phrase").size() > 1) {
+                        $(".phrase :last").remove();
+                        $(".phrase :last .word :last").remove();
+                    } else {
+                        return;
+                    }
+                } else {
+                    $(".phrase :last .word :last").remove();
                 }
-            });
-            $(".menuitem").on("click", handler);
-        }
-        if (url in cache) {
-            update(cache[url]);
-        } else {
-            $("#loading").tmpl().appendTo(".phrase :last").button();
 
-            $.ajax(
-                    {
-                        url : url,
-                        statusCode : {
-                            504 : function() {
-                                $(".loading").remove();
-                                if (i <= 1) {
-                                    request(url, i + 1);
-                                } else {
-                                    $("#reload").tmpl({}).appendTo(
-                                            ".phrase :last").button();
-                                    $(".reload").on("click", function() {
-                                        $(".reload").remove();
-                                        request(url, 0);
-                                    })
-                                }
-                            }
+                $(".words").remove();
+                add();
+            });
+
+            function add() {
+                var url = "/json/words/";
+                var w2 = $(".phrase :last .word :last").data("word");
+                var w1 = $(".phrase :last .word :last").prev().data("word");
+
+                if (w1 != "") {
+                    url += w1 + "/";
+                }
+                if (w2 != "") {
+                    url += w2;
+                }
+
+                request(url);
+            }
+
+            function request(url, i) {
+
+                i = typeof i !== 'undefined' ? i : 0;
+
+                function update(data) {
+                    $("#dropmenu").tmpl({
+                        wps : data
+                    }).appendTo(".phrase :last");
+                    $(".words").menu({
+                        position : {
+                            my : "top",
+                            at : "right-5 top+5"
                         }
-                    }).done(function(data) {
-                cache[url] = data;
-                $(".loading").remove();
-                update(data);
-            });
-        }
-    }
+                    });
+                    $(".menuitem").on("click", handler);
+                }
+                if (url in cache) {
+                    update(cache[url]);
+                } else {
+                    $("#loading").tmpl().appendTo(".phrase :last").button();
 
-    function handler(event) {
+                    $.ajax(
+                            {
+                                url : url,
+                                statusCode : {
+                                    504 : function() {
+                                        $(".loading").remove();
+                                        if (i <= 1) {
+                                            request(url, i + 1);
+                                        } else {
+                                            $("#reload").tmpl({}).appendTo(
+                                                    ".phrase :last").button();
+                                            $(".reload").on("click",
+                                                    function() {
+                                                        $(".reload").remove();
+                                                        request(url, 0);
+                                                    })
+                                        }
+                                    }
+                                }
+                            }).done(function(data) {
+                        cache[url] = data;
+                        $(".loading").remove();
+                        update(data);
+                    });
+                }
+            }
 
-        a = $(event.target).parents(".words");
+            function handler(event) {
 
-        var w1 = $(a).prev().data("word");
-        var w2 = $(event.target).data("word");
+                a = $(event.target).parents(".words");
 
-        $("#phraseitem").tmpl({
-            "word" : $(event.target).data("word")
-        }).appendTo(".phrase :last")
+                var w1 = $(a).prev().data("word");
+                var w2 = $(event.target).data("word");
 
-        $(a).remove();
+                $("#phraseitem").tmpl({
+                    "word" : $(event.target).data("word")
+                }).appendTo(".phrase :last")
 
-        if (w2 == ".") {
-            $(".phrase :last").after($("#phrase").tmpl());
+                $(a).remove();
+
+                if (w2 == ".") {
+                    $(".phrase :last").after($("#phrase").tmpl());
+                    add();
+                } else {
+                    add();
+                }
+            }
             add();
-        } else {
-            add();
-        }
-    }
-    add();
-})
+        })
