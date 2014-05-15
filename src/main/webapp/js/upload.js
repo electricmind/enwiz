53f2e486328660973ef779d2f943b10b66051d17
electricmind <buy@neural.ru>
2014-05-15 09:19:26 +0400

    Implement upload form by means of ajaxForm

---
+++ src/main/webapp/js/upload.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    function progress() {
+        $.ajax({
+            url : "/json/progress",
+            statusCode : {
+                504 : function() {
+                    $("#progress").text("Unavailable")
+                }
+            }
+        }).done(function(data) {
+            $("#progress").replaceWith($("#progress-tmpl").tmpl({
+                tasks : data
+            }));
+                                            if (data.length == 0) {
+             clearInterval(interval);
+             }
+        });
+    };
+    
+    var interval = setInterval(progress, 1000);
+    
+    $(".form-upload").ajaxForm({
+       success : function(responseText, statusText, xhr, $form)  {
+           clearInterval(interval);
+           interval = setInterval(progress, 1000);
+       }
+    });
+    
+});
