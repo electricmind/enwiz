77722820ee7497b52b5047079da544514751f5c9
electricmind <buy@neural.ru>
2014-05-16 22:36:48 +0400

    Rewrite with $.tabs() plugin

--- src/main/webapp/js/init.js
+++ src/main/webapp/js/init.js
@@ -9,26 +9,5 @@ $(document).ready(function() {
         }
     });
     
-    $(".my-panes").hide();
-    $(".my-about").show();
-
-    $(".open-generate").click(function() {
-        $(".my-panes").hide();
-        $(".my-generate").show();
-    });
-    
-    $(".open-upload").click(function() {
-        $(".my-panes").hide();
-        $(".my-upload").show();
-    });
-    
-    $(".open-memento").click(function() {
-        $(".my-panes").hide();
-        $(".my-memento").show();
-    });
-    
-    $(".open-about").click(function() {
-        $(".my-panes").hide();
-        $(".my-about").show();
-    });
+    $("#tabs").tabs();
 })
