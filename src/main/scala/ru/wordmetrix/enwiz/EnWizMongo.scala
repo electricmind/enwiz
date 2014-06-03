f67ce9956ddc460ef1520677eaa71ac07a103c91
electricmind <buy@neural.ru>
2014-06-03 13:14:52 +0400

    Implement mechanic to update database

--- src/main/scala/ru/wordmetrix/enwiz/EnWizMongo.scala
+++ src/main/scala/ru/wordmetrix/enwiz/EnWizMongo.scala
@@ -11,31 +11,59 @@ import java.io.File
  * MongoDB access data comes from ~/enwizdb.cfg
  */
 
+object EnWizMongo {
+    
+}
+
 trait EnWizMongo {
     val MO = MongoDBObject
 
     val cfg = ConfigFactory.parseFile(
-            new File(System.getProperty("user.home"),"enwizdb.cfg")
-     ).getObject("mongo").toConfig()
-    
+        new File(System.getProperty("user.home"), "enwizdb.cfg")
+    ).getObject("mongo").toConfig()
+
     val host: String = cfg.getString("host")
     val dbname: String = cfg.getString("dbname")
     val user: String = cfg.getString("user")
     val password: String = cfg.getString("password")
     val port: String = cfg.getString("port")
-    
+
     lazy val coll = MongoClient(MongoClientURI(
         s"mongodb://$user:$password@$host:$port/$dbname")
     )(dbname)("words")
-    
-    ensureIndexes
-    
-    def ensureIndexes = {
-        coll.ensureIndex(MO("word1" -> 1))
-        coll.ensureIndex(MO("word2" -> 1))
-        coll.ensureIndex(MO("word3" -> 1))
-        coll.ensureIndex(MO("word1" -> 1, "word2" -> 1))
-        coll.ensureIndex(MO("word1" -> 1, "word2" -> 1, "word3" -> 1))
-        coll.ensureIndex(MO("staat" -> 1))
+
+    def version() = {
+        coll.findOne("stat" $eq "version") match {
+            case Some(stat) => stat.getOrElse("value", 0)
+            case None => 0
+        }
     }
+
+    def version(version: Integer) = {
+        coll.update(
+            "stat" $eq "version",
+            $set("value" -> version),
+            upsert = true
+        )
+    }
+
+    def update : Unit = EnWizMongo.synchronized  {
+        println(s"Update # ${version()} $dbname")
+        version() match {
+            case 0 =>
+                coll.ensureIndex(MO("word1" -> 1))
+                coll.ensureIndex(MO("word2" -> 1))
+                coll.ensureIndex(MO("word3" -> 1))
+                coll.ensureIndex(MO("word1" -> 1, "word2" -> 1))
+                coll.ensureIndex(MO("word1" -> 1, "word2" -> 1, "word3" -> 1))
+                coll.ensureIndex(MO("stat" -> 1))
+                version(1)
+                update
+                
+            case _ =>
+                println(s"Update completed")
+        }
+    }
+    
+    update
 }
